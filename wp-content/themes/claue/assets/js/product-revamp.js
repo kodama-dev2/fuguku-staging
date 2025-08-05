b1a7706373b048b715747d41aa14a44072cc1768
kodama-dev <kodama-dev@protonmail.com>
2025-08-06 05:17:04 +0700

    v2.3.7: Added hover icons - eye icon untuk quick view dan cart icon untuk add to cart button di shop page

--- wp-content/themes/claue/assets/js/product-revamp.js
+++ wp-content/themes/claue/assets/js/product-revamp.js
@@ -1,7 +1,7 @@
 /**
  * Product Revamp Layout JavaScript
- * Version: 2.3.6
- * Description: Disable zoom hover for revamp layout, enable click-only zoom, fix multiple lightbox issue with simple modal, update button styling, pill shape buttons
+ * Version: 2.3.7
+ * Description: Disable zoom hover for revamp layout, enable click-only zoom, fix multiple lightbox issue with simple modal, update button styling, pill shape buttons, add hover icons
  */
 
 jQuery(document).ready(function($) {
